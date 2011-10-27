10a9a0fc34999a849d4ee0fdd8334a6ffd0b50e6
AOKI Hanae <aereal@kerare.org>
2011-10-28 04:58:32 +0900

    extracted to get date/time from each actions

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -1,19 +1,21 @@
 Hated.controllers do
+	before :day, :section do
+		@date = Date.parse(params[:date])
+	end
+
 	get :index do
 		@days = Section.all.group_by {|sec| sec.created_on }
 		render :index
 	end
 
 	get :day, map: '/:date' do
-		date = Date.parse(params[:date])
 		@sections = Section.all(created_on: date)
-		render :day, locals: {date: date, sections: @sections}
+		render :day, locals: {date: @date, sections: @sections}
 	end
 
 	get :section, map: '/:date/:time' do
-		date = Date.parse(params[:date])
 		time = Time.at(params[:time].to_i).to_datetime
-		@section = Section.first(created_on: date, created_at: time)
+		@section = Section.first(created_on: @date, created_at: time)
 		render :section, locals: {section: @section}
 	end
 end
